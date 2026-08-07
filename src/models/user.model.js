d4d806ef6638e97afe6ad39f3d86a16a49517c80
KRISHBODA <krishboda18@gmail.com>
2026-08-07 12:02:33 +0530

    feat: initialize backend server with Express, MongoDB connection, and user authentication model

---
+++ src/models/user.model.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: [true, "email is required of creating user"],
+        trim: true,
+        lowercase: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            "invalid email address"
+        ],
+        unique: [true, "email already exits"]
+    },
+    name: {
+        type: String,
+        required: [true, "name is required of creating user"],
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "password is required for creating account"],
+        minlength: [6, "password should contain more then 6 characters"],
+        select: false
+    }
+}, {
+    timestamps: true
+})
+
+
+userSchema.pre("save", async function(next) {
+    
+    if(!this.isModidfied("password")) {
+        return next();
+    }
+
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+    return next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+
+    return await bcrypt.compare(password,this.password)
+}
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = userModel
