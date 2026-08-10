2044993152692227c1589ac2d43e028df1b1a90a
KRISHBODA <krishboda18@gmail.com>
2026-08-10 11:06:15 +0530

    feat: implement user registration and authentication routes with JWT and cookie support

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -28,15 +28,15 @@ const userSchema = new mongoose.Schema({
 })
 
 
-userSchema.pre("save", async function(next) {
+userSchema.pre("save", async function() {
     
-    if(!this.isModidfied("password")) {
-        return next();
+    if(!this.isModified("password")) {
+        return
     }
 
     const hash = await bcrypt.hash(this.password, 10)
     this.password = hash
-    return next();
+    return 
 });
 
 userSchema.methods.comparePassword = async function (password) {
---
+++ src/routes/auth.routh.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const authController = require("../controllers/auth.controller");
+
+const router = express.Router()
+
+/* POST /api/auth/register */ 
+router.post("/register", authController.userRegisterController)
+
+/* Post /api/auth/login */
+router.post("/login", authController.userLoginController)
+
+
+
+module.exports = router
